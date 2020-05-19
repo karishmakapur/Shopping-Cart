f3d45c1cb179b45dd8fcbb9b1a4173525f21ece2
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2020-05-19 10:23:37 -0700

    initial push

---
+++ shoppingcart/src/main/java/com/karishma/shoppingcart/CmsShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.karishma.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsShoppingCartApplication.class, args);
+	}
+
+}
